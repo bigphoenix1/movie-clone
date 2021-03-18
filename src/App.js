16aac86f0bb79ba31e3bfb465e9dd55f86cee0e9
SB Jeong <bigphoenix1@gmail.com>
2021-03-18 20:11:09 +0900

    #2 JSX & PROPS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+//#2 JSX & PROPS
 import React from "react";
 import PropTypes from "prop-types";
 
-const foods = [
+const foodILike = [
   {
     id: 1,
-    name: "kimchi",
-    image: "https://cdn.imweb.me/upload/S201905105cd4e5325582d/0b667240284bd.jpg",
-    rating: 2.4,
+    name: "Kimchi",
+    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
     id: 2,
-    name: "ramyeon",
-    image: "http://newsimg.hankookilbo.com/2014/11/13/201411130948240783_1.jpg",
-    rating: 2.1,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9,
   },
   {
     id: 3,
-    name: "samgyeopsal",
+    name: "Bibimbap",
     image:
-      "https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F222d88e5c7dc496c8e8a8a56c3452e52.JPG",
-    rating: 1.6,
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8,
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5,
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7,
   },
 ];
 
 function Food({ name, picture, rating }) {
   return (
     <div>
-      <h1>I like {name}</h1>
+      <h2>I like {name}</h2>
       <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
@@ -36,13 +49,13 @@ function Food({ name, picture, rating }) {
 Food.propTypes = {
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  rating: PropTypes.number,
 };
 
 function App() {
   return (
     <div>
-      {foods.map((dish) => (
+      {foodILike.map((dish) => (
         <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
